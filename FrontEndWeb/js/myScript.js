f8cbd80e4ba380fbfd94df0f30220c23e782208d
Eduardo Merino <A01630088@itesm.mx>
2017-04-22 15:59:42 -0500

    added DEMO firebase functionality

--- FrontEndWeb/js/myScript.js
+++ FrontEndWeb/js/myScript.js
@@ -1,3 +1,17 @@
+/* Initialize Firebase */
+// Edit this to connect to the right Firebase
+  var config = {
+    apiKey: "AIzaSyCrsmS_BIUNR64gGpgGBqcjnyzGk5gy4rM",
+    authDomain: "testeatgo.firebaseapp.com",
+    databaseURL: "https://testeatgo.firebaseio.com",
+    projectId: "testeatgo",
+    storageBucket: "testeatgo.appspot.com",
+    messagingSenderId: "285087391808"
+  };
+  firebase.initializeApp(config);
+  const database = firebase.database();
+/* END Initialize Firebase */
+
 /* Get Geolocation
  * Only works on HTTPS
  */
@@ -27,19 +41,42 @@ function getLocation() {
       position = null;
     }
 }
-
-//Call function whith this
-getLocation();
 /* END Get Geolocation */
 
 /* Append Restaurant */
 //We need to fill dose variables with data from firebase.
 //We need to call this function inside a recursion to
 //load all restaurants.
+function addRestaurant(restaurantName, foodType, image, pageLink){
+  $("#RestaurantListElement").append("" +
+    "<div class='col-lg-3 col-md-4 col-xs-6 thumb'><a class='thumbnail' href='"+
+    pageLink + "'>" + "<img class='img-responsive' src='" + image + "' alt='"+
+    restaurantName +"'> "+ "<h3>" + restaurantName + "</h3><h4>" + foodType
+    + "</h4></a></div>");
+}
+
+//Go throug restaurants and display them
+function displayRestaurants(){
+  var dbRestaurantList = database.ref("Restaurant").orderByKey();
+  var restaurantName = "";
+  var foodType = "";
+  var image = "";
+  var pageLink = "";
+  dbRestaurantList.once("value")
+  .then(function(snapshot){
+    snapshot.forEach(function(childSnapshot){
+      restaurantName = childSnapshot.child("restaurantName").val();
+      foodType = childSnapshot.child("foodType").val();
+      image = childSnapshot.child("image").val();
+      pageLink = childSnapshot.child("pageLink").val();
+
+      addRestaurant(restaurantName, foodType, image, pageLink);
+    });
+  });
+}
+
 $(document).ready(function(){
-  $("#RestaurantList").append.('' +
-    '<div class="col-lg-3 col-md-4 col-xs-6 thumb"><a class="thumbnail" href="' +
-    pageLink + '">' + '<img class="img-responsive" src="' + image + '" alt="">' +
-    '<h3>' + restaurantName + '</h3><h4>' + foodType + '</h4></a></div>'
-  +'');
-});
+    displayRestaurants();
+    getLocation();
+  }
+);
