c82ba34bf2e71fada474ec1e154cdc7eb3ca384b
Eduardo Merino <A01630088@itesm.mx>
2017-04-24 13:45:15 -0500

    started vendors js logic

--- FrontEndWeb/js/vendorScript.js
+++ FrontEndWeb/js/vendorScript.js
@@ -13,21 +13,31 @@
 /* END Initialize Firebase */
 
 //ADD order
-function addOrder(orderID){
+function addOrder(orderID, clientName){
   $("#ordersList").prepend("" +
-  "<div class='col-lg-12 col-md-12 col-xs-12 Order' id=''++''><div class='row'>"+
-      "<div class='col-lg-6 col-md-6 col-xs-6'><h4 class='customerName'> To: Client</h4>"+
-        "<ul class='orderItemList' id=''+'orderItem'+orderID+''></ul>"+
-      "</div><div class='col-lg-3 col-md-3 col-xs-3' style='padding: 5px'><h4 class='customerName'> Status</h4>"+
+    "<div class='col-lg-12 col-md-12 col-xs-12 Order'>"+
+    "<div class='row'>"+
+      "<div class='col-lg-6 col-md-6 col-xs-6'>"+
+        "<h4 class='customerName'> To: Client</h4>"+
+        "<ul class='orderItemList' id='"+"itemList"+orderID+"'>"+
+        "</ul>"+
+      "</div>"+
+      "<div class='col-lg-3 col-md-3 col-xs-3' style='padding: 5px'>"+
+        "<h4 class='customerName'> Status</h4>"+
         "<div class='btn-group-vertical' role='group' aria-label='...'>"+
-          "<button type='button' class='btn btn-success' onClick='cooking_click(this.id)' id='"+"btnCooking"+orderID+"'>COOKING</button>"+
-          "<button type='button' class='btn btn-warning' onClick='onitsway_click(this.id)' id='"+"btnOnItsWay"+orderID+"'>ON ITS WAY</button>"+
-          "<button type='button' class='btn btn-danger' onClick='delivered_click(this.id)' id='"+"btnDelivered"+orderID+"'>DELIVERED</button>"+
-        "</div></div>"+
-      "<div class='col-lg-3 col-md-3 col-xs-3' style='padding: 5px; word-wrap: break-word;''>"+
+          "<button type='button' class='btn btn-success preparingOrder' id='"+orderID+"'>COOKING</button>"+
+          "<button type='button' class='btn btn-warning preparingOrder' id='"+orderID+">ON ITS WAY</button>"+
+          "<button type='button' class='btn btn-danger preparingOrder' id='"+orderID+"'>DELIVERED</button>"+
+        "</div>"+
+      "</div>"+
+      "<div class='col-lg-3 col-md-3 col-xs-3' style='padding: 5px; word-wrap: break-word;'>"+
         "<h4 class='customerName'>Assign Delivery Person</h4>"+
-        "<select style='width: 100%; position: center; padding: 10px' id='"+"selectDP"+orderID+"'>"+
-        "</select><h4>Deliver to: "+ database.ref(orderID).child("dir").val()+"<h4></div></div></div>");
+        "<select style='width: 100%; position: center; padding: 10px' class='DeliverySelect' id='+'select'+orderID+'>"+
+          "</select>"+
+          "<h4>Deliver to: "+clientName+"<h4>"+
+          "</div>"+
+        "</div>"+
+      "</div>");
 }
 
 function cooking_click(btnid){
@@ -53,3 +63,13 @@ function delivered_click(btnid){
   //..
 
 }
+
+function displayOrders(){
+  dbOrdersList = database.ref("Restaurant/"+resID).orderByKey;
+  var orderID = "";
+  var clientName = "";
+}
+
+$("document").ready(function(){
+
+});
